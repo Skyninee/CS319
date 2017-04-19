be7597eccfecb27a74e49ee497fc5cdbd0a17675
Skyninee <pongsathon.jong@bumail.net>
2017-04-20 03:59:40 +0700

    jkhkujhik

---
+++ LAB14_STU/W13_reg_action1.php
@@ -0,0 +1,54 @@
+<?php
+   $servername = "localhost";
+   $username = "root";
+   $password = "123456";
+   $dbname = "week14DB";
+
+   $name = $_POST['username'];
+   $pwd = $_POST['password'];
+   $fname = $_POST['fname'];
+   $lname = $_POST['lname'];
+
+   // Create connection
+   $conn = mysqli_connect($servername, $username, $password, $dbname);
+   
+   // Check connection
+   if (!$conn) {
+         die("Connection failed: " . mysqli_connect_error());
+   }
+   
+   mysqli_set_charset($conn,"utf8");
+   //query for check username is already exist
+   $sql = "select *from user where username = '$name'";
+   
+   $result = mysqli_query($conn,$sql);
+   $num = mysqli_num_rows($result);
+   if($num > 0){
+       echo "The username is already exist.";
+   }
+   else
+   {  
+      $sql = "select * from user where fname = '$fname' and lname = '$lname' ";
+      $result = mysqli_query($conn,$sql);
+      $num = mysqli_num_rows($result);
+      if ($num > 0)
+      {
+         echo "This name is registered!";
+      }
+      else
+      {  //write query string to insert data into user table
+         $sql="insert into user(username,password,fname,lname) values('$name','$pwd','$fname','$lname')";
+
+         $result = mysqli_query($conn,$sql);
+         if($result)
+         {
+            echo "Registration successfully inserted";            
+            echo "<meta http-equiv='refresh' content='2;url=index.html'/>";            
+         }
+         else
+         {
+            echo "Ooops...There is some problem in the registration system.";
+         }
+      }
+   }
+?>
